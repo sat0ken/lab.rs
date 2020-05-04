56e9e1978eb9bf538de558296e300a23aedced78
sat0ken <15720506+sat0ken@users.noreply.github.com>
2020-05-04 15:38:03 +0900

    add files

---
+++ comment.rs
@@ -0,0 +1,15 @@
+fn main() {
+
+    /*
+    Note: The previous column of `*` was entirely for style. There's
+    no actual need for it.
+    */
+
+    // You can manipulate expressions more easily with block comments
+    // than with line comments. Try deleting the comment delimiters
+    // to change the result:
+
+    let x = 5 + /*90 + */ 5;
+    println!("Is `x` 10 or 100? x = {}", x);
+
+}
