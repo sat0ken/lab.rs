fa89369e4147ec6bd7f9534b30737ea123f5d4a3
sat0ken <15720506+sat0ken@users.noreply.github.com>
2020-05-06 04:25:27 +0900

    add files

---
+++ 8-flow-control/dist_tuple.rs
@@ -0,0 +1,10 @@
+fn main() {
+    let pair = (1, 1);
+    println!("Tell me about {:?}", pair);
+
+    match pair {
+        (0, y) => println!("First is `0` and `y` is `{:?}`", y),
+        (x, 0) => println!("`x` is `{:?}` and last is `0`", x),
+        _ => println!("It doesn't matter what they are"),
+    }
+}
